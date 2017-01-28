9e85ac812133fd7397ad69843e7f7ec65ab41f0d
Olaf Veerman <olaf.veerman@gmail.com>
2017-01-27 23:17:52 -0500

    No tests = no problem. Travis can continue the build, even though there are no tests yet. Add linting

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ const metaConcelhos = fs.readFileSync('./data/concelhos.csv')
 const dataTaxis = fs.readFileSync('./data/taxis.csv')
 
 // Process the base data with concelhos
-var processConcelhos = function(concelhos) {
+var processConcelhos = function (concelhos) {
   return concelhos.map(c => {
     return {
       'id': parseInt(c.dico),
@@ -23,7 +23,7 @@ var processConcelhos = function(concelhos) {
 
 // Add taxi data to a concelho
 // each concelho has taxi data about a number of different indicators
-var addTaxiData = function(c, taxiData) {
+var addTaxiData = function (c, taxiData) {
   taxiData
     .filter(o => c.id === parseInt(o.dico))
     .map(o => {
@@ -42,40 +42,40 @@ var addTaxiData = function(c, taxiData) {
 }
 
 async.parallel([
-  function(callback) {
+  function (callback) {
     // Parse metadata about the concelhos
-    parse(metaConcelhos, {columns: true}, function(err, output) {
+    parse(metaConcelhos, {columns: true}, function (err, output) {
       let areas = processConcelhos(output)
       callback(err, areas)
     })
   },
-  function(callback) {
+  function (callback) {
     // Parse source data with taxis by concelho
-    parse(dataTaxis, {columns: true}, function(err, output) {
+    parse(dataTaxis, {columns: true}, function (err, output) {
       callback(err, output)
     })
   }
 ],
-function(err, results) {
+function (err, results) {
   if (err) { console.log(err.message) }
 
   var concelhoData = results[0]
   var taxiData = results[1]
 
   async.waterfall([
-    function(callback) {
+    function (callback) {
       // Add taxi data for each concelho
       concelhoData.map(c => addTaxiData(c, taxiData))
       callback(null, concelhoData)
     },
-    function(data, callback) {
+    function (data, callback) {
       // aggregate data for each district + nut
       callback(null, data)
     }
-  ], function(err, data) {
+  ], function (err, data) {
     if (err) { console.log(err.message) }
-    
+
     fs.writeFileSync('./data.json', JSON.stringify(data))
     console.log('Done!')
   })
-});
+})
